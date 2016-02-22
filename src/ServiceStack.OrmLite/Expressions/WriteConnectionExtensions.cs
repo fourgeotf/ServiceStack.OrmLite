f1ddb18378d2230ccee6929be72b5b80c9a324f3
fourgeotf <frederic.fourgeot@outlook.com>
2016-02-22 16:03:24 +0100

    Update WriteConnectionExtensions.cs
    Add IOrmLiteSession management

--- src/ServiceStack.OrmLite/Expressions/WriteConnectionExtensions.cs
+++ src/ServiceStack.OrmLite/Expressions/WriteConnectionExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Data;
 using System.Linq.Expressions;
 
@@ -6,81 +6,81 @@ namespace ServiceStack.OrmLite
 {
     public static class WriteConnectionExtensions
     {
-        public static int UpdateOnly<T>(this IDbConnection dbConn, T model, Func<SqlExpressionVisitor<T>, SqlExpressionVisitor<T>> onlyFields)
+        public static int UpdateOnly<T>(this IOrmLiteSession session, T model, Func<SqlExpressionVisitor<T>, SqlExpressionVisitor<T>> onlyFields)
         {
-            return dbConn.Exec(dbCmd => dbCmd.UpdateOnly(model, onlyFields));
+            return session.Exec(dbCmd => dbCmd.UpdateOnly(model, onlyFields));
         }
 
-        public static int UpdateOnly<T>(this IDbConnection dbConn, T model, SqlExpressionVisitor<T> onlyFields)
+        public static int UpdateOnly<T>(this IOrmLiteSession session, T model, SqlExpressionVisitor<T> onlyFields)
         {
-            return dbConn.Exec(dbCmd => dbCmd.UpdateOnly(model, onlyFields));
+            return session.Exec(dbCmd => dbCmd.UpdateOnly(model, onlyFields));
         }
 
-        public static int UpdateOnly<T, TKey>(this IDbConnection dbConn, T obj,
+        public static int UpdateOnly<T, TKey>(this IOrmLiteSession session, T obj,
             Expression<Func<T, TKey>> onlyFields = null,
             Expression<Func<T, bool>> where = null)
         {
-            return dbConn.Exec(dbCmd => dbCmd.UpdateOnly(obj, onlyFields, where));
+            return session.Exec(dbCmd => dbCmd.UpdateOnly(obj, onlyFields, where));
         }
 
-        public static int UpdateNonDefaults<T>(this IDbConnection dbConn, T item, Expression<Func<T, bool>> where)
+        public static int UpdateNonDefaults<T>(this IOrmLiteSession session, T item, Expression<Func<T, bool>> where)
         {
-            return dbConn.Exec(dbCmd => dbCmd.UpdateNonDefaults(item, where));
+            return session.Exec(dbCmd => dbCmd.UpdateNonDefaults(item, where));
         }
 
-        public static int Update<T>(this IDbConnection dbConn, T item, Expression<Func<T, bool>> where)
+        public static int Update<T>(this IOrmLiteSession session, T item, Expression<Func<T, bool>> where)
         {
-            return dbConn.Exec(dbCmd => dbCmd.Update(item, where));
+            return session.Exec(dbCmd => dbCmd.Update(item, where));
         }
 
-        public static int Update<T>(this IDbConnection dbConn, object updateOnly, Expression<Func<T, bool>> where = null)
+        public static int Update<T>(this IOrmLiteSession session, object updateOnly, Expression<Func<T, bool>> where = null)
         {
-            return dbConn.Exec(dbCmd => dbCmd.Update(updateOnly, where));
+            return session.Exec(dbCmd => dbCmd.Update(updateOnly, where));
         }
 
-        public static int Update<T>(this IDbConnection dbConn, string set = null, string where = null)
+        public static int Update<T>(this IOrmLiteSession session, string set = null, string where = null)
         {
-            return dbConn.Exec(dbCmd => dbCmd.Update<T>(set, where));
+            return session.Exec(dbCmd => dbCmd.Update<T>(set, where));
         }
 
-        public static int Update(this IDbConnection dbConn, string table = null, string set = null, string where = null)
+        public static int Update(this IOrmLiteSession session, string table = null, string set = null, string where = null)
         {
-            return dbConn.Exec(dbCmd => dbCmd.Update(table, set, where));
+            return session.Exec(dbCmd => dbCmd.Update(table, set, where));
         }
 
-        public static void InsertOnly<T>(this IDbConnection dbConn, T obj, Func<SqlExpressionVisitor<T>, SqlExpressionVisitor<T>> onlyFields)
+        public static void InsertOnly<T>(this IOrmLiteSession session, T obj, Func<SqlExpressionVisitor<T>, SqlExpressionVisitor<T>> onlyFields)
         {
-            dbConn.Exec(dbCmd => dbCmd.InsertOnly(obj, onlyFields));
+            session.Exec(dbCmd => dbCmd.InsertOnly(obj, onlyFields));
         }
 
-        public static void InsertOnly<T>(this IDbConnection dbConn, T obj, SqlExpressionVisitor<T> onlyFields)
+        public static void InsertOnly<T>(this IOrmLiteSession session, T obj, SqlExpressionVisitor<T> onlyFields)
         {
-            dbConn.Exec(dbCmd => dbCmd.InsertOnly(obj, onlyFields));
+            session.Exec(dbCmd => dbCmd.InsertOnly(obj, onlyFields));
         }
 
-        public static int Delete<T>(this IDbConnection dbConn, Expression<Func<T, bool>> where)
+        public static int Delete<T>(this IOrmLiteSession session, Expression<Func<T, bool>> where)
         {
-            return dbConn.Exec(dbCmd => dbCmd.Delete(where));
+            return session.Exec(dbCmd => dbCmd.Delete(where));
         }
 
-        public static int Delete<T>(this IDbConnection dbConn, Func<SqlExpressionVisitor<T>, SqlExpressionVisitor<T>> where)
+        public static int Delete<T>(this IOrmLiteSession session, Func<SqlExpressionVisitor<T>, SqlExpressionVisitor<T>> where)
         {
-            return dbConn.Exec(dbCmd => dbCmd.Delete(where));
+            return session.Exec(dbCmd => dbCmd.Delete(where));
         }
 
-        public static int Delete<T>(this IDbConnection dbConn, SqlExpressionVisitor<T> where)
+        public static int Delete<T>(this IOrmLiteSession session, SqlExpressionVisitor<T> where)
         {
-            return dbConn.Exec(dbCmd => dbCmd.Delete(where));
+            return session.Exec(dbCmd => dbCmd.Delete(where));
         }
 
-        public static int Delete<T>(this IDbConnection dbConn, string where = null)
+        public static int Delete<T>(this IOrmLiteSession session, string where = null)
         {
-            return dbConn.Exec(dbCmd => dbCmd.Delete<T>(where));
+            return session.Exec(dbCmd => dbCmd.Delete<T>(where));
         }
 
-        public static int Delete(this IDbConnection dbConn, string table = null, string where = null)
+        public static int Delete(this IOrmLiteSession session, string table = null, string where = null)
         {
-            return dbConn.Exec(dbCmd => dbCmd.Delete(table, where));
+            return session.Exec(dbCmd => dbCmd.Delete(table, where));
         }         
     }
 }
